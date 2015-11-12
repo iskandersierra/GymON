69cc965a9e4fc407ad74f2328b3bdc3521f15fac
iskandersierra <iskander.sierra.zaldivar@gmail.com>
2015-11-12 17:48:33 +0100

    Added DbSource but will start new branch for it

---
+++ DataUtils/DbSource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataUtils
+{
+	/// <summary>
+	/// This class allows to define a complete history of the schema of a database
+	/// to keep control of versions and upgrades
+	/// It will keep a list of versions of the database and will update it whenever possible
+	/// </summary>
+	public class DbSource
+	{
+		public DbSource ()
+		{
+		}
+	}
+}
+
